030a5872fb5e67b4c5fb1988c684c8cb604c19a8
林 暁安 <dawn8898@gmail.com>
2018-07-23 09:54:52 +0900

    Metadata config

---
+++ src/config/metadata.js
@@ -0,0 +1,548 @@
+module.exports = {  
+  /*"installedPackages":{  
+     "xmlName":"InstalledPackage",
+     "label":"",
+     "label_ja":"",
+     "children":{  
+     }
+  },*/
+  "labels":{  
+     "xmlName":"CustomLabels",
+     "label":"Custom Label",
+     "label_ja":"カスタムラベル",
+     "children":{  
+        "CustomLabels":"CustomLabel"
+     }
+  },
+  "staticresources":{  
+     "xmlName":"StaticResource",
+     "label":"Static Resource",
+     "label_ja":"静的リソース",
+     "children":{  
+
+     }
+  },
+  "scontrols":{  
+     "xmlName":"Scontrol",
+     "label":"S-Control",
+     "label_ja":"Sコントロール",
+     "children":{  
+
+     }
+  },
+  "components":{  
+     "xmlName":"ApexComponent",
+     "label":"Visualforce Component",
+     "label_ja":"Visualforce コンポーネント",
+     "children":{  
+
+     }
+  },
+  "customMetadata":{
+     "xmlName":"CustomMetadata",
+     "label":"Custom Metadata Type",
+     "label_ja":"カスタムメタデータ型",
+     "children":{  
+
+     }
+  },
+  "globalValueSets":{
+     "xmlName":"GlobalValueSet",
+     "label":"",
+     "label_ja":"",
+     "children":{  
+
+     }
+  },
+  "globalValueSetTranslations":{
+     "xmlName":"GlobalValueSetTranslation",
+     "label":"",
+     "label_ja":"",
+     "children":{  
+
+     }
+  },
+  "standardValueSets":{
+     "xmlName":"StandardValueSet",
+     "label":"",
+     "label_ja":"",
+     "children":{  
+
+     }
+  },
+  "pages":{  
+     "xmlName":"ApexPage",
+     "label":"",
+     "label_ja":"",
+     "children":{  
+
+     }
+  },
+  "queues":{  
+     "xmlName":"Queue",
+     "label":"",
+     "label_ja":"",
+     "children":{  
+
+     }
+  },
+  "objects":{  
+     "xmlName":"CustomObject",
+     "label":"",
+     "label_ja":"",
+     "children":{  
+        "actionOverrides":{"typeName":"ActionOverride","name":"actionName"},
+        "fields":{"typeName":"CustomField","name":"fullName"},
+        "businessProcesses":{"typeName":"BusinessProcess","name":"fullName"},
+        "recordTypes":{"typeName":"RecordType","name":"fullName"},
+        "webLinks":{"typeName":"WebLink","name":"fullName"},
+        "validationRules":{"typeName":"ValidationRule","name":"fullName"},
+        "namedFilters":{"typeName":"NamedFilter","name":"fullName"},
+        "sharingReasons":{"typeName":"SharingReason","name":"fullName"},
+        "listViews":{"typeName":"ListView","name":"fullName"},
+        "fieldSets":{"typeName":"FieldSet","name":"fullName"},
+        "compactLayouts":{"typeName":"CompactLayout","name":"fullName"}
+     }
+  },
+  "reportTypes":{  
+     "xmlName":"ReportType",
+     "label":"",
+     "label_ja":"",
+     "children":{  
+
+     }
+  },
+  "reports":{  
+     "xmlName":"Report",
+     "label":"",
+     "label_ja":"",
+     "children":{  
+
+     }
+  },
+  "dashboards":{  
+     "xmlName":"Dashboard",
+     "label":"",
+     "label_ja":"",
+     "children":{  
+
+     }
+  },
+  "analyticSnapshots":{  
+     "xmlName":"AnalyticSnapshot",
+     "label":"",
+     "label_ja":"",
+     "children":{  
+
+     }
+  },
+  "layouts":{  
+     "xmlName":"Layout",
+     "label":"",
+     "label_ja":"",
+     "children":{  
+
+     }
+  },
+  "portals":{  
+     "xmlName":"Portal",
+     "label":"",
+     "label_ja":"",
+     "children":{  
+
+     }
+  },
+  "documents":{  
+     "xmlName":"Document",
+     "label":"",
+     "label_ja":"",
+     "children":{  
+
+     }
+  },
+  "weblinks":{  
+     "xmlName":"CustomPageWebLink",
+     "label":"",
+     "label_ja":"",
+     "children":{  
+
+     }
+  },
+  "quickActions":{  
+     "xmlName":"QuickAction",
+     "label":"",
+     "label_ja":"",
+     "children":{  
+
+     }
+  },
+  "flexipages":{  
+     "xmlName":"FlexiPage",
+     "label":"",
+     "label_ja":"",
+     "children":{  
+
+     }
+  },
+  "tabs":{  
+     "xmlName":"CustomTab",
+     "label":"",
+     "label_ja":"",
+     "children":{  
+
+     }
+  },
+  "customApplicationComponents":{  
+     "xmlName":"CustomApplicationComponent",
+     "label":"",
+     "label_ja":"",
+     "children":{  
+
+     }
+  },
+  "applications":{  
+     "xmlName":"CustomApplication",
+     "label":"",
+     "label_ja":"",
+     "children":{  
+
+     }
+  },
+  "letterhead":{  
+     "xmlName":"Letterhead",
+     "label":"",
+     "label_ja":"",
+     "children":{  
+
+     }
+  },
+  "email":{  
+     "xmlName":"EmailTemplate",
+     "label":"",
+     "label_ja":"",
+     "children":{  
+
+     }
+  },
+  "workflows":{  
+     "xmlName":"Workflow",
+     "label":"",
+     "label_ja":"",
+     "children":{  
+        "alerts":{"typeName":"WorkflowAlert","name":"fullName"},
+        "tasks":{"typeName" : "WorkflowTask", "name" : "fullName"},
+        "outboundMessages":{"typeName" : "WorkflowOutboundMessage","name" : "fullName"},
+        "fieldUpdates":{"typeName" : "WorkflowFieldUpdate", "name" : "fullName"},
+        "rules":{"typeName" : "WorkflowRule", "name" : "fullName"},
+        "emailRecipients":{"typeName" : "WorkflowEmailRecipient", "name" : "fullName"},
+        "timeTriggers":{"typeName" : "WorkflowTimeTrigger", "name" : "fullName"},
+        "actionReferences":{"typeName" : "WorkflowActionReference", "name" : "fullName"}
+     }
+  },
+  "assignmentRules":{  
+     "xmlName":"AssignmentRules",
+     "label":"",
+     "label_ja":"",
+     "children":{  
+
+     }
+  },
+  "autoResponseRules":{  
+     "xmlName":"AutoResponseRules",
+     "label":"",
+     "label_ja":"",
+     "children":{  
+
+     }
+  },
+  "escalationRules":{  
+     "xmlName":"EscalationRules",
+     "label":"",
+     "label_ja":"",
+     "children":{  
+
+     }
+  },
+  "roles":{  
+     "xmlName":"Role",
+     "label":"",
+     "label_ja":"",
+     "children":{  
+
+     }
+  },
+  "groups":{  
+     "xmlName":"Group",
+     "label":"",
+     "label_ja":"",
+     "children":{  
+
+     }
+  },
+  "postTemplates":{  
+     "xmlName":"PostTemplate",
+     "label":"",
+     "label_ja":"",
+     "children":{  
+
+     }
+  },
+  "approvalProcesses":{  
+     "xmlName":"ApprovalProcess",
+     "label":"",
+     "label_ja":"",
+     "children":{  
+
+     }
+  },
+  "homePageComponents":{  
+     "xmlName":"HomePageComponent",
+     "label":"",
+     "label_ja":"",
+     "children":{  
+
+     }
+  },
+  "homePageLayouts":{  
+     "xmlName":"HomePageLayout",
+     "label":"",
+     "label_ja":"",
+     "children":{  
+
+     }
+  },
+  "objectTranslations":{  
+     "xmlName":"CustomObjectTranslation",
+     "label":"",
+     "label_ja":"",
+     "children":{  
+
+     }
+  },
+  "flows":{  
+     "xmlName":"Flow",
+     "label":"",
+     "label_ja":"",
+     "children":{  
+
+     }
+  },
+  "classes":{  
+     "xmlName":"ApexClass",
+     "label":"",
+     "label_ja":"",
+     "children":{  
+
+     }
+  },
+  "triggers":{  
+     "xmlName":"ApexTrigger",
+     "label":"",
+     "label_ja":"",
+     "children":{  
+
+     }
+  },
+  "profiles":{  
+     "xmlName":"Profile",
+     "label":"",
+     "label_ja":"",
+     "children":{  
+
+     }
+  },
+  "permissionsets":{  
+     "xmlName":"PermissionSet",
+     "label":"",
+     "label_ja":"",
+     "children":{  
+
+     }
+  },
+  "datacategorygroups":{  
+     "xmlName":"DataCategoryGroup",
+     "label":"",
+     "label_ja":"",
+     "children":{  
+
+     }
+  },
+  "remoteSiteSettings":{  
+     "xmlName":"RemoteSiteSetting",
+     "label":"",
+     "label_ja":"",
+     "children":{  
+
+     }
+  },
+  "authproviders":{  
+     "xmlName":"AuthProvider",
+     "label":"",
+     "label_ja":"",
+     "children":{  
+
+     }
+  },
+  "leadSharingRules":{  
+     "xmlName":"LeadSharingRules",
+     "label":"",
+     "label_ja":"",
+     "children":{  
+
+     }
+  },
+  "campaignSharingRules":{  
+     "xmlName":"CampaignSharingRules",
+     "label":"",
+     "label_ja":"",
+     "children":{  
+
+     }
+  },
+  "caseSharingRules":{  
+     "xmlName":"CaseSharingRules",
+     "label":"",
+     "label_ja":"",
+     "children":{  
+
+     }
+  },
+  "contactSharingRules":{  
+     "xmlName":"ContactSharingRules",
+     "label":"",
+     "label_ja":"",
+     "children":{  
+
+     }
+  },
+  "opportunitySharingRules":{  
+     "xmlName":"OpportunitySharingRules",
+     "label":"",
+     "label_ja":"",
+     "children":{  
+
+     }
+  },
+  "accountSharingRules":{  
+     "xmlName":"AccountSharingRules",
+     "label":"",
+     "label_ja":"",
+     "children":{  
+
+     }
+  },
+  "customObjectSharingRules":{  
+     "xmlName":"CustomObjectSharingRules",
+     "label":"",
+     "label_ja":"",
+     "children":{  
+
+     }
+  },
+  "communities":{  
+     "xmlName":"Community",
+     "label":"",
+     "label_ja":"",
+     "children":{  
+
+     }
+  },
+  "callCenters":{  
+     "xmlName":"CallCenter",
+     "label":"",
+     "label_ja":"",
+     "children":{  
+
+     }
+  },
+  "connectedApps":{  
+     "xmlName":"ConnectedApp",
+     "label":"",
+     "label_ja":"",
+     "children":{  
+
+     }
+  },
+  "samlssoconfigs":{  
+     "xmlName":"SamlSsoConfig",
+     "label":"",
+     "label_ja":"",
+     "children":{  
+
+     }
+  },
+  "synonymDictionaries":{  
+     "xmlName":"SynonymDictionary",
+     "label":"",
+     "label_ja":"",
+     "children":{  
+
+     }
+  },
+  "settings":{  
+     "xmlName":"Settings",
+     "label":"",
+     "label_ja":"",
+     "children":{  
+
+     }
+  },
+  "aura":{  
+     "xmlName":"AuraDefinitionBundle",
+     "label":"",
+     "label_ja":"",
+     "children":{  
+
+     }
+  },
+  "sharingRules":{  
+     "xmlName":"SharingRules",
+     "label":"",
+     "label_ja":"",
+     "children":{  
+        "sharingTerritoryRules":"SharingTerritoryRule",
+        "sharingOwnerRules":"SharingOwnerRule",
+        "sharingCriteriaRules":"SharingCriteriaRule"
+     }
+  },
+  "contentassets":{  
+     "xmlName":"ContentAsset",
+     "label":"",
+     "label_ja":"",
+     "children":{  
+
+     }
+  }, 
+  "networks":{  
+     "xmlName":"Network",
+     "label":"",
+     "label_ja":"",
+     "children":{  
+
+     }
+  }, 
+  "siteDotComSites":{  
+     "xmlName":"SiteDotCom",
+     "label":"",
+     "label_ja":"",
+     "children":{  
+
+     }
+  }, 
+  "flowDefinitions":{  
+     "xmlName":"FlowDefinition",
+     "label":"",
+     "label_ja":"",
+     "children":{  
+
+     }
+  },
+  "matchingRules":{  
+     "xmlName":"MatchingRules",
+     "label":"",
+     "label_ja":"",
+     "children":{  
+
+     }
+  }
+};
