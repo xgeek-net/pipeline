c2f5af2398df9bd5752630acc0fe6d01928d3da8
Xiaoan Lin <admin@xgeek.net>
2019-06-18 11:29:06 +0900

    Remove unit test on appveyor

--- build/appveyor.js
+++ build/appveyor.js
@@ -6,24 +6,14 @@ if(process.argv.length == 2) {
   process.exit(1);
 }
 // console.log(process.argv[2]);
-var clientDecode = Buffer.from(process.argv[2],'base64');
+var decode = Buffer.from(process.argv[2],'base64');
 const filePath = path.join(__dirname, '../src/config/client.js');
-var mochaDecode = Buffer.from(process.argv[3],'base64');
-const tokenFilePath = path.join(__dirname, '../test/token.js');
 // console.log(filePath);
-fs.writeFile(filePath, clientDecode, function(err) {
+fs.writeFile(filePath, decode, function(err) {
   if(err) {
     console.error(err);
     process.exit(1);
   }
   console.log('Build client.js Done!');
-  fs.writeFile(tokenFilePath, mochaDecode, function(err) {
-    if(err) {
-      console.error(err);
-      process.exit(1);
-    }
-    console.log('Build test/token.js Done!');
-    process.exit(0);
-  });
-  
+  process.exit(0);
 });
